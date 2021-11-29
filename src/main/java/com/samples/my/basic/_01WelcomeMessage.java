89dd21f8c7cf93e24f7dcccc091007be608371f8
premvinodh <premvinodh.pc@gmail.com>
2021-11-29 12:03:19 +0530

    Added _01WelcomeMessage.java

---
+++ src/main/java/com/samples/my/basic/_01WelcomeMessage.java
@@ -0,0 +1,16 @@
+/**
+ * <pre>
+ * Name			: _01WelcomeMessage.java
+ * author		: Prem Vinodh
+ * version 		:
+ * Description	: Develop a program to display a welcome message.
+ * </pre>
+ */
+package com.samples.my.basic;
+
+public class _01WelcomeMessage{
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to java... Prem");
+	}
+}
