91a37cfcf6b7c57c0bab6e22ae96b14cd0f6ba6a
premvinodh <premvinodh.pc@gmail.com>
2021-11-29 12:05:22 +0530

    Added _02WelcomeMessageOnMultipleLines.java

---
+++ src/main/java/com/samples/my/basic/_02WelcomeMessageOnMultipleLines.java
@@ -0,0 +1,18 @@
+/**
+ * <pre>
+ * Name			: _02WelcomeMessageOnMultipleLines.java
+ * author		: Prem Vinodh
+ * version 		:
+ * Description	: Develop a program to display message on multiple lines.
+ * </pre>
+ */
+package com.samples.my.basic;
+
+public class _02WelcomeMessageOnMultipleLines {
+
+	public static void main(String[] args) {
+		System.out.print("Welcome to Java");
+		System.out.print("\nFor");
+		System.out.print("\nPrem");
+	}
+}
